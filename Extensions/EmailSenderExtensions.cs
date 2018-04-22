d15978706c183bf1f337d407dd87abd9507d638d
bharath.reddy <Bharath.Reddy@LSLT-IT10440>
2018-04-22 19:44:05 +0530

    Version-3

--- Extensions/EmailSenderExtensions.cs
+++ Extensions/EmailSenderExtensions.cs
@@ -21,5 +21,14 @@ namespace Lshp.BaseWebApi.Extensions
             return emailSender.SendEmailAsync(email, "Confirm your email",
                 $"Please confirm your account by clicking this link: <a href='{HtmlEncoder.Default.Encode(link)}'>link</a>");
         }
+		
+		///T BHARATH REDDY
+		
+		public static Task SendEmailConfirmationAsync(this IEmailSender emailSender, string email, string link)
+        {
+            return emailSender.SendEmailAsync(email, "Confirm your email",
+                $"Please confirm your account by clicking this link: <a href='{HtmlEncoder.Default.Encode(link)}'>link</a>");
+        }
+		//emailSender.SendEmailAsync(email, "Confirm your email",
     }
 }
